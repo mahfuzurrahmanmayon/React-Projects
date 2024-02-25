d310e66bc90fc002fe6bf9ae5866ba9d8cb68c00
Mahfuzur Rahman <mahfujurrahman291003@gmail.com>
2024-02-25 17:27:50 +0600

    Design Completed

--- Project/text-analyzer/src/components/Analyzer.js
+++ Project/text-analyzer/src/components/Analyzer.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { FaFacebook } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
-import wegems from '../../src/images/Wegems.svg'
+import wegems from "../../src/images/Wegems.svg";
 import "./Analyzer.css";
 
 const Analyzer = () => {
   return (
-    <div>
+    <div className="Analyzer">
       <div className="hero">
         <div className="container">
           <div className="hero-Wrapper">
-            <h2>Text Analyser</h2>
+            <span>Text Analyser</span>
             <div className="hero-right">
-              <a href="https://www.wegems.co/"><img src={wegems} alt="" /></a>
-              <a href="https://www.linkedin.com/in/mahfujurrahmanmayon/">
+              <a href="https://www.wegems.co/">
+                <img src={wegems} alt="" />
+              </a>
+              <a href="https://www.facebook.com/mahfuzurrahmanmayon">
                 <FaFacebook />
               </a>
               <a href="https://www.linkedin.com/in/mahfujurrahmanmayon/">
@@ -23,30 +25,58 @@ const Analyzer = () => {
           </div>
         </div>
       </div>
-      <div className="main">
-        <div className="main-container">
-          <div className="main-wrapper">
-            <div className="analyze-text-result">
-              <div className="analyze-item">
-                <div className="text">Words</div>
-                <div className="number">5</div>
-              </div>
-              <div className="analyze-item">
-                <div className="text">Words</div>
-                <div className="number">5</div>
-              </div>
-              <div className="analyze-item">
-                <div className="text">Words</div>
-                <div className="number">5</div>
-              </div>
-              <div className="analyze-item">
-                <div className="text">Words</div>
-                <div className="number">5</div>
-              </div>
-              <div className="analyze-item">
-                <div className="text">Words</div>
-                <div className="number">5</div>
-              </div>
+      <div className="small-container">
+        <div className="main-app">
+          <div className="result-bar">
+            <div className="result-box">
+              <span className="box-title">Words</span>
+              <span className="box=value">5</span>
+            </div>
+            <div className="result-box">
+              <span className="box-title">Characters</span>
+              <span className="box=value">5</span>
+            </div>
+            <div className="result-box">
+              <span className="box-title">Sentences</span>
+              <span className="box=value">5</span>
+            </div>
+            <div className="result-box">
+              <span className="box-title">Paragraphs</span>
+              <span className="box=value">5</span>
+            </div>
+            <div className="result-box">
+              <span className="box-title">Pronouns</span>
+              <span className="box=value">5</span>
+            </div>
+          </div>
+          <textarea
+            className="text-area"
+            placeholder="Paste your text here ..."
+          ></textarea>
+          <div className="bottom-result-bar">
+            <div className="result-box">
+              <span className="box-title">Longest word:</span>
+              <span className="box-value">-</span>
+            </div>
+            <div className="result-box">
+              <span className="box-title">Average Reading Time:</span>
+              <span className="box-value">~</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="container">
+          <div className="footer-wrapper">
+            <span>Build by Mahfuz</span>
+            <div className="footer-box">
+              <a href="https://www.linkedin.com/in/mahfujurrahmanmayon/">
+                <span>About Us</span>
+              </a>
+              <span>|</span>
+              <a href="https://www.facebook.com/mahfuzurrahmanmayon">
+                <span>Contact Us</span>
+              </a>
             </div>
           </div>
         </div>
